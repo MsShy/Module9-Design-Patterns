076c2bc2b24165ad4fbf42ef9e0e4a1002bd23f3
MsShy <tanyat93@mail.ru>
2016-04-22 15:35:07 +0100

    change LoggerTest

--- src/test/java/homework/Singleton/LoggerTest.java
+++ src/test/java/homework/Singleton/LoggerTest.java
@@ -2,13 +2,14 @@ package homework.singleton;
 
 import org.junit.Test;
 
+import java.io.*;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 import static org.junit.Assert.*;
 
 public class LoggerTest {
-	Logger logger = Logger.getInstance();
+	private Logger logger = Logger.getInstance();
 
 	@Test
 	public void testGetInstance() {
@@ -16,16 +17,26 @@ public class LoggerTest {
 	}
 
 	@Test
-	public void testSerialize() {
+	public void testSerialize() throws IOException, ClassNotFoundException {
+		ObjectOutput out=new ObjectOutputStream(new FileOutputStream("src/test/resources/file.ser"));
+		out.writeObject(logger);
+		out.close();
+		ObjectInputStream in=new ObjectInputStream(new FileInputStream("src/test/resources/file.ser"));
+		Logger serializedLogger= (Logger) in.readObject();
+		in.close();
+		assertTrue(serializedLogger == Logger.getInstance());
+
 
 	}
 
 	@Test
-	public void testReflation() {
+	public void testReflation() throws IllegalAccessException, InvocationTargetException, InstantiationException {
+		Logger log = null;
 		Constructor[] constructors = Logger.class.getDeclaredConstructors();
 		for (Constructor constructor : constructors) {
 			constructor.setAccessible(true);
-			assertTrue(logger == Logger.getInstance());
+			log = (Logger) constructor.newInstance();
+			assertTrue(log == Logger.getInstance());
 		}
 	}
 
